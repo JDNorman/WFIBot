2bfd21b0f69cdceb18bb0915cc8174e9de7525d8
dnorm <dnorman1611@gmail.com>
2024-02-29 20:02:35 -0600

    x

--- commands/member.js
+++ commands/member.js
@@ -1,6 +1,10 @@
 // imports
 const { SlashCommandBuilder } = require("discord.js");
 const { DateTime } = require('luxon');
+const path = require('path');
+const configPath = path.resolve(__dirname, '..', 'config.json');
+const { color } = require(configPath);
+const decimalColor = parseInt(color, 16);
 
 module.exports = {
     //Command Builder
@@ -22,7 +26,7 @@ module.exports = {
         const member = await int.client.users.fetch(Target?.id);
         
         const Embed = {
-          color: color,
+          color: decimalColor,
           thumbnail: 
           {
                 url: Target.displayAvatarURL(),
