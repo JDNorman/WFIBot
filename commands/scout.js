22dfc3ad559e2ee097d67610c97010ce178a6a42
dnorm <dnorman1611@gmail.com>
2024-03-10 00:01:31 -0600

    x

--- commands/scout.js
+++ commands/scout.js
@@ -105,11 +105,11 @@ module.exports = {
             sponsorStr.forEach(spo => {
                 homePage.addFields({ name: '*Sponsor:*', value: spo });
         })};
-        if (countryStr != 'null') homePage.addFields({ name: '*Region:*', value: countryStr });
-        if (stateStr != 'null') homePage.addFields({ name: '*State/Province:*', value: stateStr });
-        if (cityStr != 'null') homePage.addFields({ name: '*City:*', value: cityStr });
-        if (rookieStr != 'null') homePage.addFields({ name: '*Rookie Year:*', value: rookieStr });
-        if (websiteStr != 'null') homePage.addFields({ name: '*Website:*', value: websiteStr });
+        if (countryStr != 'null') homePage.addFields({ name: '*Region:*', value: countryStr, inline: true });
+        if (stateStr != 'null') homePage.addFields({ name: '*State/Province:*', value: stateStr, inline: true });
+        if (cityStr != 'null') homePage.addFields({ name: '*City:*', value: cityStr, inline: true });
+        if (rookieStr != 'null') homePage.addFields({ name: '*Rookie Year:*', value: rookieStr, inline: true });
+        if (websiteStr != 'null') homePage.addFields({ name: '*Website:*', value: websiteStr, inline: true });
                 
         int.editReply('Embeds constructed!');
             
@@ -157,17 +157,16 @@ module.exports = {
             .setTitle('***Total Season Overview***')
             .setDescription('**' + seasonStr + ' season**');
                 
-        if (totvalStr != 'null') totSeason.addFields({ name: '*Average points scored per game:*', value: totvalStr });
+        if (totvalStr != 'null') totSeason.addFields({ name: '*Average points scored per game:*', value: totvalStr, inline: true });
         if (totrankStr != 'null') totSeason.addFields({ name: '*Season ranking by points:*', value: totrankStr });
-        if (avgautovalStr != 'null') totSeason.addFields({ name: '*Average points scored in auto:*', value: avgautovalStr });
+        if (avgautovalStr != 'null') totSeason.addFields({ name: '*Average points scored in auto:*', value: avgautovalStr, inline: true });
         if (avgautorankStr != 'null') totSeason.addFields({ name: '*Season ranking by points in auto:*', value: avgautorankStr });
-        if (avgdcvalStr != 'null') totSeason.addFields({ name: '*Average points scored in driver control:*', value: avgdcvalStr });
+        if (avgdcvalStr != 'null') totSeason.addFields({ name: '*Average points scored in driver control:*', value: avgdcvalStr, inline: true });
         if (avgdcrankStr != 'null') totSeason.addFields({ name: '*Season ranking by points in driver control:*', value: avgdcrankStr });
-        if (avgegvalStr != 'null') totSeason.addFields({ name: '*Average points scored in end game:*', value: avgegvalStr });
+        if (avgegvalStr != 'null') totSeason.addFields({ name: '*Average points scored in end game:*', value: avgegvalStr, inline: true });
         if (avgegrankStr != 'null') totSeason.addFields({ name: '*Season ranking by points in end game:*', value: avgegrankStr });
             
 
         int.followUp({ embeds: [homePage, totSeason] });
     },
 };
-
